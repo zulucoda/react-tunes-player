b83efcbe9e698558302878c6fae6f6ce13e91e10
Muzikayise Flynn Buthelezi aka zuluCoda <muzi@mfbproject.co.za>
2017-08-09 18:23:29 +0200

    clean up comments

--- src/components/react-tunes-player/index.js
+++ src/components/react-tunes-player/index.js
@@ -1,11 +1,3 @@
-// import ReactTunePlayer from './react-tunes-player-container';
-// import ReactTunePlayerReducer from './react-tunes-player-reducer';
-
-// module.exports = {
-//   ReactTunePlayer: ReactTunePlayer,
-//   ReactTunePlayerReducer: ReactTunePlayerReducer
-// };
-
 export ReactTunesPlayerContainer from "./react-tunes-player-container";
 export reactTunesPlayerReducer, {
   setPreviousTune,
