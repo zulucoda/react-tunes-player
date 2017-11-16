2c555da804880fd3ec9c04c7a2c00dd5cea86af6
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2017-11-16 21:34:28 +0200

    [muzi] updates

--- src/components/react-tunes-player/tests/react-tunes-player-container.spec.js
+++ src/components/react-tunes-player/tests/react-tunes-player-container.spec.js
@@ -6,7 +6,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import configureStore from "redux-mock-store";
-import ReactTunesPlayerContainer from "../react-tunes-player-container";
+import { ReactTunesPlayerContainer } from "../../react-tunes-player";
 import {
   setTunes,
   setCurrentTune,
