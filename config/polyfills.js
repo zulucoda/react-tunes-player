629bc2fe65c3e61cec6da876ce62141c5b8ccdac
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2018-08-23 17:59:58 +0200

    [muzi] adding enzyme adapter for config :fist:

--- config/polyfills.js
+++ config/polyfills.js
@@ -1,4 +1,8 @@
 "use strict";
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
 
 if (typeof Promise === "undefined") {
   // Rejection tracking prevents a common issue where React gets into an
