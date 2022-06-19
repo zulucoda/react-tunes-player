13ca3a9ce811d64a241fe50e3329f905482e2c99
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2022-06-19 18:30:15 +0200

    feature: updating player controls with callback funcs

--- src/components/player-controls/index.js
+++ src/components/player-controls/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import device from '../../utils/devices';
 import { SkipBack, PlayCircle, PauseCircle, SkipForward } from 'lucide-react';
+import { func } from 'prop-types';
 
 const PlayerControlsList = styled.ul`
   height: 100%;
@@ -17,27 +18,67 @@ const PlayerControlsList = styled.ul`
 
 const PlayerControlItem = styled.li`
   list-style: none;
+  cursor: pointer;
 `;
 
-const PlayerControls = () => {
+const PlayerControls = ({ play, pause, skipBack, skipForward }) => {
+  const onSkipBackClick = (e) => {
+    e.preventDefault();
+    return skipBack();
+  };
+
+  const onPlayClick = (e) => {
+    e.preventDefault();
+    return play();
+  };
+
+  const onPauseClick = (e) => {
+    e.preventDefault();
+    return pause();
+  };
+
+  const onSkipForwardClick = (e) => {
+    e.preventDefault();
+    return skipForward();
+  };
+
   return (
     <div className="player-controls">
       <PlayerControlsList className="player-controls-list">
-        <PlayerControlItem className="player-controls-list-skip-back">
+        <PlayerControlItem
+          className="player-controls-list-skip-back"
+          onClick={onSkipBackClick}
+        >
           <SkipBack className="skip-back" role="skip-back" />
         </PlayerControlItem>
-        <PlayerControlItem className="player-controls-list-play-circle">
+        <PlayerControlItem
+          className="player-controls-list-play-circle"
+          onClick={onPlayClick}
+        >
           <PlayCircle className="play-circle" role="play-circle" />
         </PlayerControlItem>
-        <PlayerControlItem className="player-controls-list-pause-circle">
-          <PauseCircle className="pause-circle" role="Pause-circle" />
+        <PlayerControlItem
+          className="player-controls-list-pause-circle"
+          onClick={onPauseClick}
+        >
+          <PauseCircle className="pause-circle" role="pause-circle" />
         </PlayerControlItem>
-        <PlayerControlItem className="player-controls-list-skip-forward">
+        <PlayerControlItem
+          className="player-controls-list-skip-forward"
+          onClick={onSkipForwardClick}
+        >
           <SkipForward className="skip-forward" role="skip-forward" />
         </PlayerControlItem>
       </PlayerControlsList>
     </div>
   );
+};
+
+PlayerControls.propTypes = {
+  skipBack: func.isRequired,
+  play: func.isRequired,
+  pause: func.isRequired,
+  skipForward: func.isRequired,
 };
 
 export default PlayerControls;
--- src/components/player-controls/index.test.js
+++ src/components/player-controls/index.test.js
@@ -1,3 +1,55 @@
+import React from 'react';
+import PlayerControls from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 describe('Components - Player Controls - Render Test', () => {
-  test('should render player controls', () => {});
+  let playMock;
+  let skipBackMock;
+  let skipForwardMock;
+  let pauseMock;
+  beforeEach(() => {
+    playMock = jest.fn();
+    skipBackMock = jest.fn();
+    skipForwardMock = jest.fn();
+    pauseMock = jest.fn();
+  });
+
+  test('should render player controls', () => {
+    render(
+      <PlayerControls
+        play={playMock}
+        skipForward={skipForwardMock}
+        pause={pauseMock}
+        skipBack={skipBackMock}
+      />,
+    );
+    expect(screen.getByRole('skip-back')).toBeInTheDocument();
+    expect(screen.getByRole('skip-forward')).toBeInTheDocument();
+    expect(screen.getByRole('play-circle')).toBeInTheDocument();
+    expect(screen.getByRole('pause-circle')).toBeInTheDocument();
+  });
+
+  test('should be able to click on all play controls', () => {
+    render(
+      <PlayerControls
+        play={playMock}
+        skipForward={skipForwardMock}
+        pause={pauseMock}
+        skipBack={skipBackMock}
+      />,
+    );
+
+    fireEvent.click(screen.getByRole('skip-back'));
+    expect(skipBackMock).toHaveBeenCalled();
+
+    fireEvent.click(screen.getByRole('skip-forward'));
+    expect(skipForwardMock).toHaveBeenCalled();
+
+    fireEvent.click(screen.getByRole('play-circle'));
+    expect(playMock).toHaveBeenCalled();
+
+    fireEvent.click(screen.getByRole('pause-circle'));
+    expect(pauseMock).toHaveBeenCalled();
+  });
 });
